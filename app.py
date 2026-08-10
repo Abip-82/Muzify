d5aab12f3dbec5f2cfb069dc0be9999d41031cfa
Abip-82 <abipmahat04@gmail.com>
2026-08-10 15:09:32 +0545

    some progress

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/form", methods=["POST"])
+def form():
+    return render_template('form.html')
+
+@app.route('/playlists', methods=["POST"])
+def playlists():
+    mood = request.form["mood"].strip().lower()
+    weather = request.form["weather"].strip().lower()
+    activities = request.form["activities"].strip().lower()
+    return render_template('playlists.html')
 
 if __name__== '__main__':
     app.run()
